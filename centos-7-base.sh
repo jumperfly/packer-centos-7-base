c0e44aaedb8f361bf1de79f77a58bbd3b94cb965
jumperfly <git@jumperfly.co.uk>
2020-07-22 21:49:44 +0100

    Add further cleanup and dns/ssh optimisations

--- centos-7-base.sh
+++ centos-7-base.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-rm -f /etc/ssh/*key*
+echo 'RES_OPTIONS="single-request-reopen"' >> /etc/sysconfig/network
+sed -i 's/^#\?UseDNS .*/UseDNS no/' /etc/ssh/sshd_config
+sed -i 's/^#\?GSSAPIAuthentication .*/GSSAPIAuthentication no/' /etc/ssh/sshd_config
 
-dd if=/dev/zero of=/boot/ZERO bs=1M
+rm -f /etc/ssh/*key*
+find /var/log -type f -exec truncate --size=0 {} \
+truncate --size=0 /etc/machine-id
+
+dd if=/dev/zero of=/boot/ZERO bs=1M || echo "ignoring expected dd error"
 rm /boot/ZERO
-dd if=/dev/zero of=/ZERO bs=1M
+dd if=/dev/zero of=/ZERO bs=1M  || echo "ignoring expected dd error"
 rm /ZERO
 sync
+
+export HISTSIZE=0
+rm -f /root/.wget-hsts
